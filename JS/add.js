b613309294eaa7415136f4f2e746042530e34392
Łukasz <lukaszk9396@gmail.com>
2019-07-18 12:51:24 +0200

    Bestsellers card imporovement

--- JS/add.js
+++ JS/add.js
@@ -1,5 +1,29 @@
-$('.add').hover(
+$('.dwa').hover(
     function () {
         // $(this).css('right', '-.5rem');
+        $(this).css("width", "30%");
+    },
+    function () {
+        // $(this).css('right', '-.5rem');
+        $(this).css("width", "15%");
     }
 );
+
+$(document).ready(function () {
+    if ($(window).width() > 1200) {
+        $(".add").css("width", $(".bestseller").height());
+        $(".add").css("height", $(".dwa").width() + "px");
+        $(".add").css("line-height", $(".dwa").width() + "px");
+    }
+
+});
+
+
+$(window).resize(function () {
+    if ($(window).width() > 1200) {
+        $(".add").css("width", $(".bestseller").height());
+        $(".add").css("height", $(".dwa").width() + "px");
+        $(".add").css("line-height", $(".dwa").width() + "px");
+
+    }
+});
